c6e513097bf54ddc65b5415f0679f06926655f67
Kasun-Abeywickrama <121012573+Kasun-Abeywickrama@users.noreply.github.com>
2024-03-22 10:31:16 +0530

    Create Question 03

---
+++ Lab - 01/Question_03/src/java/mypackage/MyServlet.java
@@ -0,0 +1,34 @@
+package mypackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author HP
+ */
+@WebServlet(name = "MyServlet", urlPatterns = {"/MyServlet"})
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         response.setContentType("text/html");
+        String fName = request.getParameter("f_name");
+        String sName = request.getParameter("s_name");
+
+        response.getWriter().println("First Name : "+fName+" Surname : "+sName);
+    }
+
+}
